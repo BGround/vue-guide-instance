c352ff272ca4baaac5060d6126f5428dabac4e06
ya_zhang <zhangya19902016@gmail.com>
2020-10-13 10:45:27 +0800

    axios参数配置，拦截器以及错误处理

---
+++ axios-vue/src/views/AxiosInstance.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="axiosinstance">
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'axiosinstance',
+  created(){
+	  let instance = axios.create({
+		  baseURL:'http://localhost:8080', //请求域名，基本地址
+		  timeout: 1000, //请求的超时时长(ms)
+		  url:'/data.json' ,//请求的路径
+		  method: '' ,//五大请求方法
+		  headers: {} ,//请求头
+		  params:{}, //请求参数，会拼接到url上
+		  data: {} //请求参数放在请求体中
+	  })
+	  
+	  //请求优先级顺序，下面是从低到高
+	  //1.axios全局配置
+	  axios.defaults.baseURL='http://localhost:8080'
+	  axios.defaults.timeout= 1000
+	  //2.axios实例配置
+	  let instance=axios.create()
+	  instance.default.timeout = 3000
+	  //3.axios请求配置
+	  instance.get('/data.json',{
+		  timeout:5000
+	  })
+	  
+	  instance.get('/data.json').then(res=>{
+		  console.log(res)
+	  })
+  }
+}
+</script>
---
+++ axios-vue/src/views/Interceptor.vue
@@ -0,0 +1,42 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+	//拦截器：在请求或者响应被处理前拦截他们
+	//请求拦截器，响应拦截器
+	import axios from 'axios'
+	export default{
+		name:'Interceptor',
+		created(){
+			//请求拦截器 以及错误处理
+			axios.interceptors.request.use(
+			config=>{
+				return config
+			},
+			err=>{
+				$(#WindowModal).show() //弹窗显示
+				setTimeout(()=>{
+					$(#WindowModal).hide()
+				},2000)
+				return Promise.reject(err)
+			})
+			
+			//响应拦截器 以及错误处理
+			axios.interceptors.respose.use(
+			res=>{
+				return res
+			},
+			err=>{
+				$(#WindowModal).show() //弹窗显示
+				setTimeout(()=>{
+					$(#WindowModal).hide()
+				},2000)
+				return Promise.reject(err)
+			})
+		}
+	}
+</script>
+
+<style>
+</style>
