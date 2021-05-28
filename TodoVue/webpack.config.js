dcf8e0e2de8a0bef20114e1a0cdab63861d3bc42
zhangya <zhangya3@mychery.com>
2021-05-28 10:21:45 +0800

    build error fix

--- TodoVue/webpack.config.js
+++ TodoVue/webpack.config.js
@@ -6,6 +6,7 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const isDev = process.env.NODE_ENV === 'development'
 
 const config = {
+    mode: isDev ? 'development' : 'production',
     target: "web",                                                       //设置webpack的编译目标是web平台
     entry: path.join(__dirname, './src/index.js'),                       //声明js文件入口,__dirname就是我们文件的根目录,用join拼接
     output: {                                                            //声明出口文件
@@ -13,7 +14,7 @@ const config = {
         path: path.join(__dirname, 'dist')
     },
     stats: {
-        children: true,
+        children: false,
         errorDetails: true
     },
     performance: { hints: false },
@@ -72,7 +73,7 @@ const config = {
         new VueLoaderPlugin()
     ]
 }
-config.devtool = isDev ? false : '#cheap-module-eval-source-map'     //官方推荐使用这个配置,作用是在浏览器中调试时,显示的代码和我们的项目中的代码会基本相似,而不会显示编译后的代码,以致于我们调试连自己都看不懂
+// config.devtool = isDev ? false : '#cheap-module-eval-source-map'     //官方推荐使用这个配置,作用是在浏览器中调试时,显示的代码和我们的项目中的代码会基本相似,而不会显示编译后的代码,以致于我们调试连自己都看不懂
 if(isDev) {
     config.devServer = {                                             //这个devServer的配置是在webpack2.x以后引入的,1.x是没有的
         port: 8000,
@@ -83,6 +84,7 @@ if(isDev) {
         // open: true ,                                              //项目启动时,会默认帮你打开浏览器
         hot: true                                                   //在单页面应用开发中,我们修改了代码后是整个页面都刷新,开启hot后,将只刷新对应的组件
     }
+    config.devtool = isDev ? false : '#cheap-module-eval-source-map' //webpack5+版本下需要严格设置'false'
     config.plugins.push(                                            //添加两个插件用于hot:true的配置
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
