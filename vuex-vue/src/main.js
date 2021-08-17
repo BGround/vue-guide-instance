0b6a11ec6d3b675d5885497180048674d77dff2e
zhangya <zhangya@mychery,com>
2021-08-17 10:35:36 +0800

    mod vue-router

--- vuex-vue/src/main.js
+++ vuex-vue/src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 
 Vue.config.productionTip = true
 
-Vue.use(Vuex)
+Vue.use(Vuex)          // Vuex运行依赖Vue内部数据的双向绑定机制
 
 router.beforeEach((to, from, next) => {
 	if(store.state.userInfo || to.path === "/login") {
@@ -18,7 +18,7 @@ router.beforeEach((to, from, next) => {
 	}
 })
 
-new Vue({
+new Vue({              // new一个Vue实例，来响应式变化
 	store,
 	router,
 	render: h => h(App),
